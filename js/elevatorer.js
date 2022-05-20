8c83bd098bd0949959574be5587fe0acd11a2c87
Yacoub Hanna <92095965+yacobhanna@users.noreply.github.com>
2022-05-20 11:30:08 +0200

    many changes

--- js/elevatorer.js
+++ js/elevatorer.js
@@ -5,15 +5,15 @@ const imgSlider = document.getElementById('img-slider');
 imgSlider.addEventListener('click', event => {
 
     if (event.target === image1) {
-         headImage.setAttribute ("src","./res/img/Elevatorer.jpg");
+         headImage.setAttribute ("src","./res/img/Enbema_elevator.jpg");
     }
 
     else if (event.target === image2) {
-      headImage.setAttribute ("src","./res/img/Elevator2.jpg");
+      headImage.setAttribute ("src","./res/img/Enbema_elevatortopp450.jpg");
     }
 
     else {
-      headImage.setAttribute ("src","./res/img/elevator3.png");
+      headImage.setAttribute ("src","./res/img/Enbema_elevatortopp_2_450.jpg");
     }
   
 });
--- js/spjäll.js
+++ js/spjäll.js
@@ -5,7 +5,7 @@ const imgSlider = document.getElementById('img-slider');
 imgSlider.addEventListener('click', event => {
 
     if (event.target === image1) {
-         headImage.setAttribute ("src","./res/img/spjäll450.jpg");
+         headImage.setAttribute ("src","./res/img/spjäll449.jpg");
     }
 
     else if (event.target === image2) {
